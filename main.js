f277e42cf901c1c8fc9461a2fd5a1dab992d3959
Eddy Yi <superywan@eddys-mbp.home>
2020-03-12 19:39:08 -0700

    second commit

---
+++ main.js
@@ -0,0 +1,56 @@
+const givenTxt = document.getElementById("givenTxt");
+const userInput = document.getElementById("userInput");
+const howManyCorrect = document.getElementById("howManyCorrect");
+
+const wordList = [
+  "jazz",
+  "jizz",
+  "fizz",
+  "fuzz",
+  "hizz",
+  "wazz",
+  "bazz",
+  "buzz",
+  "mezz",
+  "quiz"
+];
+
+let counter = 0;
+let didEnd = false;
+let correctCounter = 0;
+let i = 0;
+
+function correctChecker() {
+  // Check User typed correctly..
+  if (givenTxt.innerText === userInput.value) {
+    if (counter != 0) {
+      console.log("Correct!");
+      correctCounter++;
+      howManyCorrect.innerHTML = correctCounter;
+    }
+  } else {
+    console.log("You suck!");
+  }
+}
+
+function typeWriter() {
+  correctChecker();
+  // Empty the txtarea and move on to the next word.
+  userInput.value = "";
+  givenTxt.innerHTML = wordList[i];
+  i++;
+  counter += 1;
+}
+
+userInput.addEventListener("keydown", function(e) {
+  //checks whether the pressed key is "Enter"
+  if (e.keyCode === 13) {
+    // If Counter is over the length of wordList exit the function.
+    if (counter >= wordList.length - 1) {
+      correctChecker();
+      givenTxt.innerText = "--done--";
+      return;
+    }
+    typeWriter();
+  }
+});
